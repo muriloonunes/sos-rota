0992ff59aa08b73bf270f498807a3684b64d3fc7
Hartur <hartursalesxavier@gmail.com>
2025-11-27 22:24:07 -0300

    feat: CRUD Profissional

---
+++ src/main/java/mhd/sosrota/repository/ProfissionalRepository.java
@@ -0,0 +1,30 @@
+package mhd.sosrota.repository;
+
+import mhd.sosrota.model.Profissional;
+import mhd.sosrota.model.enums.FuncaoProfissional;
+
+import java.util.List;
+
+/**
+ * @author Hartur Sales <hartursalesxavier@gmail.com>
+ * @date 27/11/2025
+ * @brief class AmbulanciaRepositoryImpl
+ */
+
+public interface ProfissionalRepository {
+    boolean salvar(Profissional profissional);
+
+    Profissional buscarPorNome(String nome);
+
+    List<Profissional> listarTodos();
+
+    List<Profissional> listarPorFuncao(FuncaoProfissional funcao);
+
+    List<Profissional> listarAtivos();
+
+    Profissional atualizar(Profissional profissional);
+
+    boolean deletar(Profissional profissional);
+
+    //List<Profissional> listarPorEquipe(Long equipeId);
+}
---
+++ src/main/java/mhd/sosrota/repository/ProfissionalRepositoryImpl.java
@@ -0,0 +1,111 @@
+package mhd.sosrota.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+import mhd.sosrota.infrastructure.database.JpaManager;
+import mhd.sosrota.model.Profissional;
+import mhd.sosrota.model.enums.FuncaoProfissional;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Hartur Sales <hartursalesxavier@gmail.com>
+ * @date 27/11/2025
+ * @brief class ProfissionalRepositoryImpl
+ */
+
+public class ProfissionalRepositoryImpl implements ProfissionalRepository {
+
+    @Override
+    public boolean salvar(Profissional profissional) {
+        EntityManager em = JpaManager.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(profissional);
+            em.getTransaction().commit();
+            return true;
+        } catch (RuntimeException e) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            return false;
+        }
+    }
+
+    @Override
+    public Profissional buscarPorNome(String nome) {
+        try(EntityManager em = JpaManager.getEntityManager()) {
+            TypedQuery<Profissional> q = em.createQuery(
+                    "SELECT p FROM Profissional p WHERE p.nome = :nome", Profissional.class);
+            q.setParameter("nome", nome);
+            return q.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Profissional> listarTodos() {
+        try(EntityManager em = JpaManager.getEntityManager()) {
+            TypedQuery<Profissional> q = em.createQuery(
+                    "SELECT p FROM Profissional p ORDER BY p.nome", Profissional.class);
+            return q.getResultList();
+        }catch (NoResultException e) {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public List<Profissional> listarPorFuncao(FuncaoProfissional funcao) {
+        if (funcao == null) return Collections.emptyList();
+        try (EntityManager em = JpaManager.getEntityManager()){
+            TypedQuery<Profissional> q = em.createQuery(
+                    "SELECT p FROM Profissional p WHERE p.funcaoProfissional = :funcao ORDER BY p.nome",
+                    Profissional.class);
+            q.setParameter("funcao", funcao);
+            return q.getResultList();
+        }catch (NoResultException e) {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public List<Profissional> listarAtivos() {
+        try(EntityManager em = JpaManager.getEntityManager()) {
+            TypedQuery<Profissional> q = em.createQuery(
+                    "SELECT p FROM Profissional p WHERE p.ativo = true ORDER BY p.nome",
+                    Profissional.class);
+            return q.getResultList();
+        } catch (NoResultException e) {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public Profissional atualizar(Profissional profissional) {
+        try(EntityManager em = JpaManager.getEntityManager()) {
+            em.getTransaction().begin();
+            Profissional atualizado = em.merge(profissional);
+            em.getTransaction().commit();
+            return atualizado;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean deletar(Profissional profissional) {
+        try(EntityManager em = JpaManager.getEntityManager()) {
+            em.getTransaction().begin();
+            Profissional profissionalDeletar = em.find(Profissional.class, profissional.getNome());
+            if (profissionalDeletar == null) {
+                return false;
+            }
+            em.remove(profissionalDeletar);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
