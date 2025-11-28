2bba41b0141959b0fff9c86438a746d2e9137282
Hartur <hartursalesxavier@gmail.com>
2025-11-27 21:23:23 -0300

    feat: novos métodos para listar, atualizar e deletar usuários no repositório

--- src/main/java/mhd/sosrota/repository/UsuarioRepository.java
+++ src/main/java/mhd/sosrota/repository/UsuarioRepository.java
@@ -2,13 +2,19 @@ package mhd.sosrota.repository;
 
 import mhd.sosrota.model.Usuario;
 
+import java.util.List;
+
 /**
  *
  * @author Murilo Nunes <murilo_no@outlook.com>
+ * @author Hartur Sales <hartursalesxavier@gmail.com>
  * @date 25/11/2025
  * @brief Interface UsuarioRepository
  */
 public interface UsuarioRepository {
     Usuario encontrarPorUsername(String username);
+    List<Usuario> listarTodosUsuários();
     boolean salvar(Usuario usuario);
+    Usuario atualizarUsuario(Usuario usuario);
+    boolean deletarUsuario(Usuario usuario);
 }
--- src/main/java/mhd/sosrota/repository/UsuarioRepositoryImpl.java
+++ src/main/java/mhd/sosrota/repository/UsuarioRepositoryImpl.java
@@ -5,9 +5,11 @@ import jakarta.persistence.NoResultException;
 import mhd.sosrota.infrastructure.database.JpaManager;
 import mhd.sosrota.model.Usuario;
 
+import java.util.List;
+
 /**
- *
  * @author Murilo Nunes <murilo_no@outlook.com>
+ * @author Hartur Sales <hartursalesxavier@gmail.com>
  * @date 25/11/2025
  * @brief Class UsuarioRepository
  */
@@ -25,10 +27,44 @@ public class UsuarioRepositoryImpl implements UsuarioRepository {
     }
 
     @Override
+    public List<Usuario> listarTodosUsuários() {
+        try (EntityManager em = JpaManager.getEntityManager()) {
+            return em.createQuery("SELECT u FROM Usuario u", Usuario.class)
+                    .getResultList();
+        }
+    }
+
+    @Override
     public boolean salvar(Usuario usuario) {
         try (EntityManager em = JpaManager.getEntityManager()) {
             em.getTransaction().begin();
             em.persist(usuario);
+            em.getTransaction().commit();
+            return true;
+        }
+    }
+
+    @Override
+    public Usuario atualizarUsuario(Usuario usuario) {
+        try (EntityManager em = JpaManager.getEntityManager()) {
+            em.getTransaction().begin();
+            Usuario atualizado = em.merge(usuario);
+            em.getTransaction().commit();
+            return atualizado;
+        }
+    }
+
+    @Override
+    public boolean deletarUsuario(Usuario usuario) {
+        try (EntityManager em = JpaManager.getEntityManager()) {
+            em.getTransaction().begin();
+            Usuario usuarioDeletar = em.find(Usuario.class, usuario.getUsername());
+            if (usuarioDeletar == null) {
+                return false;
+            }
+
+            em.getTransaction().begin();
+            em.remove(usuarioDeletar);
             em.getTransaction().commit();
             return true;
         }
